de81fe462bc057ffdb4d2576360113c6bb3129e8
Greta Schock <gschockcodes@gmail.com>
2021-06-29 12:28:26 -0700

    Change names/routes for images

--- data.js
+++ data.js
@@ -1,73 +1,80 @@
 module.exports = {
   giideList: [
+    // {
+    //   title: 'Call for Code',
+    //   author: 'Laisha Desmuke',
+    //   duration: '14:39',
+    //   thumbnailUrl: require('../assets/images/explore/call_for_code.jpg'),
+    //   category: 'explore'
+    // },
     {
       title: 'Call for Code',
       author: 'Laisha Desmuke',
       duration: '14:39',
-      thumbnailUrl: require('../assets/images/explore/call_for_code.jpg'),
+      thumbnailUrl: require('./public/images/call_for_code.jpg'),
       category: 'explore'
     },
     {
       title: 'Declutter Your Digital Life',
       author: 'Sharon Panelo',
       duration: '21:33',
-      thumbnailUrl: require('../assets/images/explore/declutter.jpg'),
+      thumbnailUrl: require('./public/images/declutter.jpg'),
       category: 'explore'
     },
     {
       title: 'Mindfulness at Work',
       author: 'Amanda McCracken',
       duration: '5:52',
-      thumbnailUrl: require('../assets/images/explore/mindfulness.jpg'),
+      thumbnailUrl: require('./public/images/mindfulness.jpg'),
       category: 'explore'
     },
     {
       title: 'Pantone Color of the Year: 2021',
       author: 'Allison Kent-Smith',
       duration: '3:44',
-      thumbnailUrl: require('../assets/images/explore/etcetera.jpg'),
+      thumbnailUrl: require('./public/images/etcetera.jpg'),
       category: 'explore'
     },
     {
       title: 'Your Place, My Place, Our Place',
       author: 'Lori Kent',
       duration: '6:35',
-      thumbnailUrl: require('../assets/images/explore/your_place.jpg'),
+      thumbnailUrl: require('./public/images/your_place.jpg'),
       category: 'explore'
     },
     {
       title: 'Welcome to the Creator Beta',
       author: 'Allison Kent-Smith',
       duration: '1:49',
-      thumbnailUrl: require('../assets/images/explore/welcome_creator_beta.jpg'),
+      thumbnailUrl: require('./public/images/welcome_creator_beta.jpg'),
       category: 'learn'
     },
     {
       title: 'Why Giide',
       author: 'Allison Kent-Smith',
       duration: '1:43',
-      thumbnailUrl: require('../assets/images/explore/why_giide.jpg'),
+      thumbnailUrl: require('./public/images/why_giide.jpg'),
       category: 'learn'
     },
     {
       title: 'Make a Chapter Zing',
       author: 'Allison Kent-Smith',
       duration: '1:40',
-      thumbnailUrl: require('../assets/images/explore/chapter_zing.png'),
+      thumbnailUrl: require('./public/images/chapter_zing.png'),
       category: 'learn'
     },
     {
       title: 'Meet the Content Feed',
       author: 'Allison Kent-Smith',
       duration: '1:44',
-      thumbnailUrl: require('../assets/images/explore/content_feed.jpg'),
+      thumbnailUrl: require('./public/images/content_feed.jpg'),
       category: 'learn'
     },
     {
       title: 'Why Shorter is Better',
       author: 'Allison Kent-Smith',
       duration: '1:16',
-      thumbnailUrl: require('../assets/images/explore/shorter_better.png'),
+      thumbnailUrl: require('./public/images/shorter_better.png'),
       category: 'learn'
     }
   ],
