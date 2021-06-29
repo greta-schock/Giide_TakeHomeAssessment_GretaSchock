4942fc13c5a1f68f11ab84d99c8279602c9c884a
Greta Schock <gschockcodes@gmail.com>
2021-06-29 13:20:08 -0700

    Added file-loader

--- client/App.jsx
+++ client/App.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import giideList from '../data.js'
 
 const App = () => {
     return (
+        <>
+        {console.log(giideList)}
         <div>App is up and running!</div>
+        </>
     )
 }
 
