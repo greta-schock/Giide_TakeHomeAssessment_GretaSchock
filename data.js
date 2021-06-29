58373c02213898fb6e92cde2b48e68a8e6477140
Greta Schock <gschockcodes@gmail.com>
2021-06-29 12:24:26 -0700

    Added files from Giide

---
+++ data.js
@@ -0,0 +1,74 @@
+module.exports = {
+  giideList: [
+    {
+      title: 'Call for Code',
+      author: 'Laisha Desmuke',
+      duration: '14:39',
+      thumbnailUrl: require('../assets/images/explore/call_for_code.jpg'),
+      category: 'explore'
+    },
+    {
+      title: 'Declutter Your Digital Life',
+      author: 'Sharon Panelo',
+      duration: '21:33',
+      thumbnailUrl: require('../assets/images/explore/declutter.jpg'),
+      category: 'explore'
+    },
+    {
+      title: 'Mindfulness at Work',
+      author: 'Amanda McCracken',
+      duration: '5:52',
+      thumbnailUrl: require('../assets/images/explore/mindfulness.jpg'),
+      category: 'explore'
+    },
+    {
+      title: 'Pantone Color of the Year: 2021',
+      author: 'Allison Kent-Smith',
+      duration: '3:44',
+      thumbnailUrl: require('../assets/images/explore/etcetera.jpg'),
+      category: 'explore'
+    },
+    {
+      title: 'Your Place, My Place, Our Place',
+      author: 'Lori Kent',
+      duration: '6:35',
+      thumbnailUrl: require('../assets/images/explore/your_place.jpg'),
+      category: 'explore'
+    },
+    {
+      title: 'Welcome to the Creator Beta',
+      author: 'Allison Kent-Smith',
+      duration: '1:49',
+      thumbnailUrl: require('../assets/images/explore/welcome_creator_beta.jpg'),
+      category: 'learn'
+    },
+    {
+      title: 'Why Giide',
+      author: 'Allison Kent-Smith',
+      duration: '1:43',
+      thumbnailUrl: require('../assets/images/explore/why_giide.jpg'),
+      category: 'learn'
+    },
+    {
+      title: 'Make a Chapter Zing',
+      author: 'Allison Kent-Smith',
+      duration: '1:40',
+      thumbnailUrl: require('../assets/images/explore/chapter_zing.png'),
+      category: 'learn'
+    },
+    {
+      title: 'Meet the Content Feed',
+      author: 'Allison Kent-Smith',
+      duration: '1:44',
+      thumbnailUrl: require('../assets/images/explore/content_feed.jpg'),
+      category: 'learn'
+    },
+    {
+      title: 'Why Shorter is Better',
+      author: 'Allison Kent-Smith',
+      duration: '1:16',
+      thumbnailUrl: require('../assets/images/explore/shorter_better.png'),
+      category: 'learn'
+    }
+  ],
+};
