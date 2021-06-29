1442ff030fe9a909a989fcafb90ff48528e5747b
Greta Schock <gschockcodes@gmail.com>
2021-06-28 18:21:07 -0700

    Server changes to get App up and running

--- server/index.js
+++ server/index.js
@@ -1,10 +1,15 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
+const axios = require('axios');
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const PUBLIC_DIR = path.resolve(__dirname, '..', 'public');
+
+app.use(express.json());
+app.use(morgan('dev'));
+app.use(express.static(PUBLIC_DIR));
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
