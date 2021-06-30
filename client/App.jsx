1556409da13dccf13dad701e587636964d923427
Greta Schock <gschockcodes@gmail.com>
2021-06-29 19:56:33 -0700

    Reformatted GiideCard with new href

--- client/App.jsx
+++ client/App.jsx
@@ -8,9 +8,9 @@ const App = () => {
     var learnGiideList = giideList.filter(giideItem => giideItem.category === 'learn');
 
     return (
-        <div className="explore_exploreGiideList">
+        <div className="explore_exploreGiideList__1R6Iy">
             <h2>Explore</h2>
-            <div className="explore_cardContainer">
+            <div className="explore_cardContainer__1FiHX explore_cardContainerFeatured__2IzXs">
                 {exploreGiideList.map((giideItem, index) => (
                     <GiideCard
                         key={index + giideItem.title}
