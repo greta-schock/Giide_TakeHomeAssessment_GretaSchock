65332a876fbb594e5597d99185517944ba05cfc5
Greta Schock <gschockcodes@gmail.com>
2021-07-01 11:05:32 -0700

    Removed axios and deactivated morgan for final presentation

--- server/index.js
+++ server/index.js
@@ -1,14 +1,13 @@
 const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
-const axios = require('axios');
 const app = express();
 const port = 3000;
 
 const PUBLIC_DIR = path.resolve(__dirname, '..', 'public');
 
 app.use(express.json());
-app.use(morgan('dev'));
+// app.use(morgan('dev'));
 app.use(express.static(PUBLIC_DIR));
 
 app.listen(port, () => {
